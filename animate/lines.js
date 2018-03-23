f32a5437a768aee137a24a58c4d107f9c48d5ae5
owenroberts <obr208@nyu.edu>
2018-03-23 00:41:33 -0400

    trying to figure out layout fuck everything

--- animate/lines.js
+++ animate/lines.js
@@ -7,13 +7,12 @@ window.addEventListener("load", function() {
 	Lines.lines = []; // lines currently being drawn
 	Lines.drawings = []; // saved drawings
 	Lines.currentFrame = 0;
-
 	Lines.interface = new Interface();
-	Lines.canvas = new Canvas(512, 512, "ffffff"); // width height color, def color is 000000/black
+	Lines.canvas = new Canvas(512, 512, "000000"); // width height color, def color is 000000/black
 	Lines.draw = new Draw();
 	Lines.data = new Data();
 	Lines.color = new Color("color", "Line Color"); 
-	// Lines.color.setColor("ff00ff");
+	Lines.color.setColor("ffffff");
 	Lines.drawEvents = new DrawEvents();
 	Lines.fio = new Files_IO();
 	Lines.draw.start();
