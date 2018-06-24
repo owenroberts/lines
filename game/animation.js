fe94dbe523565ed7024eccecdb5a7e671791b1cb
owenroberts <obr208@nyu.edu>
2018-06-24 12:24:23 -0400

    classy anim and sprite

--- game/animation.js
+++ game/animation.js
@@ -1,76 +1,91 @@
 /* animation is the lines playing module, child of sprite obj */
-function Animation(src) {
-	const self = this;
-	const ctx = Game.ctx;
-	const mixedColors = Game.mixedColors;
-	this.debug = false;
-	this.loaded = false;
-	this.play = true;
-	this.src = src;
-	this.frames = [];
-	this.drawings = [];
-	this.currentFrame = 0; 
-	this.currentFrameCounter = 0;  // floats
-	this.widthRatio = 1;
-	this.heightRatio = 1;
-	this.loop = true;
-	
-	this.frameCount = -1; // i guess this is set at beginning? set to 0 so it doesn't go through
-	this.frameCountCounter = -1;  // floats
-	
-	// this.playOnce = false; // this is a trigger?  should probably be a method?
-	
-	this.intervalRatio = 1;
-	this.mirror = false;
-	this.states = {
-		'default': {start: 0, end: 0 }
-	}
-	this.state = 'default';
+class Animation {
+	constructor(src) {
+		this.src = src;
+		this.ctx = Game.ctx;
+		this.mixedColors = Game.mixedColors;
+		this.debug = false;
+		this.loaded = false;
+		this.drawBackground = false;
 
-	this.load = function(setAnimSize, callback) {
+		this.isPlaying = true;
+
+		this.frames = [];
+		this.drawings = [];
+		this.currentFrame = 0;
+		this.currentFrameCounter = 0;  // floats
+		this.widthRatio = 1;
+		this.heightRatio = 1;
+
+		this.loop = true;
+		this.intervalRatio = 1;
+
+		/* figure this out now, with states this is very different */
+		this.frameCount = -1; // i guess this is set at beginning? set to 0 so it doesn't go through
+		this.frameCountCounter = -1;  // floats
+
+		this.state = 'default';
+		this.states = { 'default': {start: 0, end: 0 } }
+
+		// this.mirror = false; /* not implemented */
+	}
+
+
+	load(setAnimSize, callback) {
 		fetch(this.src)
 			.then(response => { return response.json() })
 			.then(data => {
-				self.loaded = true;
-				self.frames = data.f;
-				self.drawings = data.d;
-				if (self.states.default)
-					self.states.default.end = self.frames.length;
+				this.loaded = true;
+				this.frames = data.f;
+				this.drawings = data.d;
+				if (this.states.default)
+					this.states.default.end = this.frames.length;
 				if (!setAnimSize) {
 					if (callback) 
 						callback(data.w, data.h);
 				} else {
-					self.widthRatio = setAnimSize.w / data.w;
-					self.heightRatio = setAnimSize.h / data.h;
+					this.widthRatio = setAnimSize.w / data.w;
+					this.heightRatio = setAnimSize.h / data.h;
 				}
-				self.intervalRatio = Game.lineInterval / (1000/data.fps);
+				this.intervalRatio = Game.lineInterval / (1000/data.fps);
 			});
-	};
+	}
 
-	this.createNewState = function(label, start, end) {
+	createNewState(label, start, end) {
 		if (!this.states[label]) {
 			this.states[label] = {
 				start: start,
 				end: end
 			}
 		}
-		this.state = label;
+		this.setState(label);
 	}
 
-	this.playOnce = function(callback) {
-		if (this.debug) console.log(this.state, this.states[this.state].start)
+	setState(state) {
+		if (this.state != state) {
+			this.state = state;
+			this.currentFrame = this.currentFrameCounter = this.states[this.state].start;
+		}
+	}
+
+	/* need to work on this
+		in drummer game it plays and then disappears
+		in other scenarios is should play and stick ...
+		but that should be loop */
+	playOnce(callback) {
 		this.frameCount = this.states[this.state].start;
 		this.frameCountCounter = this.states[this.state].start;
 		this.playOnceCallback = callback;
 	}
 
-	this.endPlayOnce = function() {
-		this.frameCount = -1;
+	/* playOnce should end itself */
+	endPlayOnce() {
+		this.frameCount = -1; /* this is stupid */
 		this.frameCountCounter = -1;
 	}
 
-	this.draw = function(x, y) {
-		if (this.frameCount == -1) { /* frameCount -1 is default, means its looping */
+	draw(x, y) {
+		if (this.frameCount == -1) { /* frameCount -1 is default, means it plays from beginning */
 			if (this.debug) {
 				// console.log(this.state, this.states[this.state])
 				// console.log(this.currentFrame, this.currentFrameCounter);
@@ -104,122 +119,88 @@ function Animation(src) {
 			}
 		}
 
-		if (this.loaded && this.play) {
+		if (this.loaded && this.isPlaying) {
 			if (this.mirror) {
-				ctx.save();
-				//ctx.translate(this.width, 0);
-				//ctx.scale(-1,1);
+				// this.ctx.save();
+				// ctx.translate(this.width, 0);
+				// ctx.scale(-1,1);
 			}
 			if (this.frames[this.currentFrame]) {
-				if (!mixedColors) 
-					ctx.beginPath();
+				if (!this.mixedColors) 
+					this.ctx.beginPath();
 				for (let i = 0; i < this.frames[this.currentFrame].length; i++) {
 					const fr = this.frames[this.currentFrame][i];
 					const jig = +fr.r;
 					const seg = +fr.n;
 					const dr = this.drawings[fr.d];
-					if (mixedColors) 
-						ctx.beginPath();
+					if (this.mixedColors) 
+						this.ctx.beginPath();
 					for (let h = fr.s; h < fr.e - 1; h++) {
 						const s = dr[h]; // line data
 						const e = dr[h + 1];
 						let v = new Cool.Vector(e.x, e.y);
 						v.subtract(s);
 						v.divide(seg); // line num
-						ctx.moveTo(
+						this.ctx.moveTo(
 							x + this.widthRatio * (fr.x + s.x + Cool.random(-jig, jig)), 
 							y + this.heightRatio * (fr.y + s.y + Cool.random(-jig, jig)) 
 						);
 						for (let j = 0; j < seg; j++) {
 							const p = new Cool.Vector(s.x + v.x * j, s.y + v.y * j);
-							ctx.lineTo( 
+							this.ctx.lineTo( 
 								x + this.widthRatio * (fr.x + p.x + v.x + Cool.random(-jig, jig)), 
 								y + this.heightRatio * (fr.y +  p.y + v.y + Cool.random(-jig, jig)) 
 							);
 						}
-						if (ctx.strokeStyle.replace("#","") != fr.c) {
-							ctx.strokeStyle= "#" + fr.c;
+						if (this.ctx.strokeStyle.replace("#","") != fr.c) {
+							this.ctx.strokeStyle= "#" + fr.c;
 						}
 					}
-					if (mixedColors) ctx.stroke();
+					if (this.mixedColors) this.ctx.stroke();
 				}
-				if (!mixedColors) ctx.stroke();
+				if (!this.mixedColors) this.ctx.stroke();
+				if (this.drawBackground)
+					this.drawBkg(x, y);
 			}
-			if (this.mirror) ctx.restore();
-		}
-	};
-
-	this.setState = function(state) {
-		if (this.debug) console.log(state);
-		if (this.state != state) {
-			this.state = state;
-			this.currentFrame = this.currentFrameCounter = this.states[this.state].start;
+			// if (this.mirror) this.ctx.restore();
 		}
 	}
 
-	this.drawBkg = function(x, y) {
-		if (this.frameCount > -1) {
-			if (this.playOnce && this.frameCountRounded < this.frames.length) {
-				this.frameCount += this.intervalRatio;
-				this.frameCountRounded = Math.floor(this.frameCount);
-			}
-			if (this.frameCountRounded >= this.frames.length) {
-				this.playOnce = false;
-				this.frameCount = this.frameCountRounded = 0;
-			}
-			this.currentFrame = this.currentFrameRounded = this.frameCountRounded;
-		} else {
-			if (this.currentFrameRounded < this.frames.length) {
-				this.currentFrame  += this.intervalRatio;
-				this.currentFrameRounded = Math.floor(this.currentFrame);
-			}
-			if (this.currentFrameRounded >= this.frames.length) {
-				if (this.loop) this.currentFrame = this.currentFrameRounded = 0;
-				else this.currentFrame = this.currentFrameRounded = this.frames.length - 1;
-			}
-		}
-
-		if (this.loaded && this.play) {
-			if (this.frames[this.currentFrameRounded]) {
-				if (!mixedColors)
-					ctx.beginPath();
-				for (let i = 0; i < this.frames[this.currentFrameRounded].length; i++) {
-					const frm = this.frames[this.currentFrameRounded][i];
-					const drw = this.drawings[frm.d];
-					if (mixedColors)
-						ctx.beginPath();
-					for (let h = frm.i; h < frm.e; h++) {
-						const line = drw.l[h]; // line data
-						if (line && line.e) {
-							const v = new Cool.Vector(line.e.x, line.e.y);
-							ctx.lineTo(
-								x + this.widthRatio * (line.s.x + Cool.random(-this.jiggle, this.jiggle)), 
-								y + this.heightRatio * (line.s.y + Cool.random(-this.jiggle, this.jiggle)) 
-							);
-							
-							if (ctx.fillStyle.replace("#","") != drw.c) {
-								ctx.fillStyle = "#" + drw.c;
-							}
-						}
-					}
-					if (mixedColors) {
-						ctx.closePath();
-						ctx.fill();
-
+	drawBkg(x, y) {
+		if (!this.mixedColors)
+			this.ctx.beginPath();
+		for (let i = 0; i < this.frames[this.currentFrameRounded].length; i++) {
+			const frm = this.frames[this.currentFrameRounded][i];
+			const drw = this.drawings[frm.d];
+			if (mixedColors)
+				this.ctx.beginPath();
+			for (let h = frm.i; h < frm.e; h++) {
+				const line = drw.l[h]; // line data
+				if (line && line.e) {
+					const v = new Cool.Vector(line.e.x, line.e.y);
+					this.ctx.lineTo(
+						x + this.widthRatio * (line.s.x + Cool.random(-this.jiggle, this.jiggle)), 
+						y + this.heightRatio * (line.s.y + Cool.random(-this.jiggle, this.jiggle)) 
+					);
+					if (this.ctx.fillStyle.replace("#","") != drw.c) {
+						this.ctx.fillStyle = "#" + drw.c;
 					}
 				}
-				if (!mixedColors)
-					ctx.fill();
+			}
+			if (this.mixedColors) {
+				this.ctx.closePath();
+				this.ctx.fill();
 			}
 		}
-	};
+		if (!this.mixedColors)
+			this.ctx.fill();
+	}
 
-	this.stop = function() {
-		self.play = false;
-	};
+	stop() {
+		this.isPlaying = false;
+	}
 
-	this.start = function() {
-		self.play = true;
-	};
-
+	start() {
+		this.isPlaying = true;
+	}
 }
