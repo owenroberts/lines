83546f40bb2bb3ceb9e2c4c9bb7c53f99895b4b6
owenroberts <obr208@nyu.edu>
2018-07-29 16:18:22 -0400

    fit canvas param

--- animate/lines.js
+++ animate/lines.js
@@ -10,13 +10,15 @@ window.addEventListener("load", function() {
 	// modules 
 	Lines.interface = new Interface();
 	// width height color, def color is black 000000
-	Lines.canvas = new Canvas(512, 512, "ffffff" ); 
+	Lines.canvas = new Canvas(1024, 768, "ffffff" ); 
 	Lines.draw = new Draw();
 	Lines.data = new Data();
 	Lines.lineColor = new Color("color", "Line Color"); 
 	/*Lines.lineColor.setColor("ffffff");*/
 	Lines.drawEvents = new DrawEvents();
-	Lines.fio = new Files_IO();
+	Lines.fio = new Files_IO({
+		fit: false
+	});
 	
 	// start, for some reason
 	Lines.draw.start();
