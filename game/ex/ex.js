25e21d51dd0ddfd87019fa7cdad806557df15b64
owenroberts <obr208@nyu.edu>
2019-04-17 13:27:27 -0400

    game anim

--- game/ex/ex.js
+++ game/ex/ex.js
@@ -16,7 +16,7 @@ Game.scenes = {
 	'drag': {} /* not implemented */
 };
 Game.currentScene = 'walk';
-Game.initLettering('letters.json'); // create letters for each game
+// Game.initLettering('letters.json'); // create letters for each game
 
 /* usually in spearate data files */
 const title = new Text(10, 40, "welcome to the game", 10, Game.letters);
@@ -41,7 +41,7 @@ class Character extends Sprite {
 	constructor(x, y) {
 		super(x, y);
 		this.speed = new Cool.Vector(0, 0); /* sprite physics has velocity ... */
-		this.addAnimation('wiggle_char.json');
+		this.addAnimation('/sprite.json');
 	}
 
 	update() {
@@ -65,8 +65,8 @@ function draw() {
 		character.display();
 	});
 
-	title.display(); // lettering example
-	joinGame.display(); // ui example
+	// title.display(); // lettering example
+	// joinGame.display(); // ui example
 }
 
 function update() {
@@ -79,15 +79,19 @@ function update() {
 function keyDown(key) {
 	switch (key) {
 		case 'a':
+		case 'left':
 			char.speed.x = -5;
 			break;
 		case 'd':
+		case 'right':
 			char.speed.x = 5;
 			break;
 		case 'w':
+		case 'up':
 			char.speed.y = -5;
 			break;
 		case 's':
+		case 'down':
 			char.speed.y = 5;
 			break;
 	}
@@ -96,15 +100,19 @@ function keyDown(key) {
 function keyUp(key) {
 	switch (key) {
 		case 'a':
+		case 'left':
 			char.speed.x = 0;
 			break;
 		case 'd':
+		case 'right':
 			char.speed.x = 0;
 			break;
 		case 'w':
+		case 'up':
 			char.speed.y = 0;
 			break;
 		case 's':
+		case 'down':
 			char.speed.y = 0;
 			break;
 	}
