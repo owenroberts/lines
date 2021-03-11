09cec1130ef40edeb7cd2e1b31efb2adce167b96
owenroberts <obroberts@gmail.com>
2021-03-11 17:22:49 -0500

    fixed drawing module

--- animate/interface/drawings.js
+++ animate/interface/drawings.js
@@ -1,26 +1,34 @@
 function Drawings() {
 	const self = this;
 
-	this.getLayer = function(index) {
-		const layers = [];
-		// - 1 for draw layer
+	function getLayer(drawingIndex) {
 		// get layers containing drawing
-		for (let i = 0; i < lns.anim.layers.length - 1; i++) {
-			if (lns.anim.layers[i].drawingIndex == index) layers.push(lns.anim.layers[i]);
-		}
+		// this would be easier if layrs inside of drawing ...
+		const layers = lns.anim.layers
+			.filter((layer, index) => index < lns.anim.layers.length - 1 && layer.drawingIndex == drawingIndex);
+		// console.log(drawingIndex, layers);
 
-		if (layers.length == 0) return false;
-		else if (layers.length == 1) return layers[0];
-		else {
+		if (layers.length > 0) {
 			// get a layer in the current frame if it exists
 			for (let i = 0; i < layers.length; i++) {
 				if (layers[i].isInFrame(lns.anim.currentFrame)) {
 					return layers[i];
 				}
 			}
+
+			// or get adjacent layer
+			for (let i = 0; i < layers.length; i++) {
+				if (layers[i].isInFrame(lns.anim.currentFrame + 1) || 
+					layers[i].isInFrame(lns.anim.currentFrame - 1)) {
+					return layers[i];
+				}
+			}
+
 			// or just return the first layer
 			return layers[0];
 		}
+		// console.log(layers);
+		return false;
 	};
 
 	this.update = function() {
@@ -28,41 +36,48 @@ function Drawings() {
 		// -1 to ignore draw drawing
 		for (let i = 0; i < lns.anim.drawings.length - 1; i++) {
 			if (lns.anim.drawings[i]) {
-				const drawing = lns.anim.drawings[i];
-				let layer = self.getLayer(i); /* check for existing layer */
-				console.log(layer);
-				let inFrame = layer ? layer.isInFrame(lns.anim.currentFrame) : false;
+				// const drawing = lns.anim.drawings[i];
+				let layer = getLayer(i); /* check for existing layer */
 
 				self.panel.drawings.append(new UIToggle({
 					text: i,
-					isOn: inFrame,
+					isOn: layer ? layer.isInFrame(lns.anim.currentFrame) : false,
 					callback: function() {
-						let layer = self.getLayer(i);
+						let layer = getLayer(i);
 
-						/* add */
-						if (!this.isOn) {
+						
+						if (!this.isOn) { /* add */
+							console.log(layer);
 							if (layer) {
-								layer = layer.addIndex(lns.anim.currentFrame);
+								if (layer.isInFrame(lns.anim.currentFrame) ||
+									layer.isInFrame(lns.anim.currentFrame - 1) ||
+									layer.isInFrame(lns.anim.currentFrame + 1)) {
+									layer.addIndex(lns.anim.currentFrame);
+								} else {
+									const props = layer.getCloneProps();
+									props.startFrame = props.endFrame = lns.anim.currentFrame;
+									lns.anim.addLayer(new Layer(props));
+								}
 							} else {
-								layer = new Layer({
-									...drawing.props,
-									d: i,
-									f: { s: lns.anim.currentFrame, e: lns.anim.currentFrame },
-								});
-								delete drawing.props;
-								lns.anim.addLayer(layer);
+								lns.anim.addLayer(new Layer({
+									drawingIndex: i,
+									linesInterval: +lns.ui.faces.linesInterval.value,
+									segmentNum: +lns.ui.faces.segmentNum.value,
+									jiggleRange: +lns.ui.faces.jiggleRange.value,
+									wiggleRange: +lns.ui.faces.wiggleRange.value,
+									wiggleSpeed: +lns.ui.faces.wiggleSpeed.value,
+									color: lns.ui.faces.color.value,
+									startFrame: lns.anim.currentFrame,
+								}));
 							}
-						} else {
+						} else { /* remove */
 							if (layer) {
 								if (layer.isInFrame(lns.anim.currentFrame)) {
-									const newLayer = layer.removeIndex(lns.anim.currentFrame);
-									
-									if (newLayer == 'remove') {
-										drawing.props = layer.props; // is this bad?
+									const newLayer = layer.removeIndex(lns.anim.currentFrame, function() {
 										lns.anim.removeLayer(layer);
-									}
-									else if (newLayer) {
-										lns.anim.addLayer(layer);
+									});
+									if (newLayer) {
+										lns.anim.addLayer(newLayer);
 									}
 								} 
 							} 
