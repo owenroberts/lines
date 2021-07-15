94f6ba35f0bb4544f39f40e8481236a5004a5840
owenroberts <obroberts@gmail.com>
2021-07-15 14:03:24 -0400

    fixed view

--- game/classes/SHGScene.js
+++ game/classes/SHGScene.js
@@ -9,23 +9,24 @@ class SHGScene extends Scene {
 		const h = bounds.bottom - bounds.top;
 		this.grid = new SpatialHashGrid(bounds, w / width, h / height);
 		this.view = [width, height]; // seems nuts but cant get offset right ... 
+		this.clients = [];
 	}
 
 	addSprite(sprite) {
 		this.grid.addSprite(sprite);
 	}
 
-	display(position) {
-		const clients = this.grid.findNear(position[0] - this.view[0] / 2, position[1] - this.view[1] / 2, this.view[0], this.view[1]);
-		for (let i = 0; i < clients.length; i++) {
-			clients[i].sprite.display();
+	display() {
+		// const clients = this.grid.findNear(position[0] - this.view[0] / 2, position[1] - this.view[1] / 2, this.view[0], this.view[1]);
+		for (let i = 0; i < this.clients.length; i++) {
+			this.clients[i].sprite.display();
 		}
 	}
 
 	update(offset, position) {
-		const clients = this.grid.findNear(position[0] - this.view[0] / 2, position[1] - this.view[1] / 2, this.view[0], this.view[1]);
-		for (let i = 0; i < clients.length; i++) {
-			clients[i].sprite.update(offset);
+		this.clients = this.grid.findNear(position[0] - this.view[0] / 2, position[1] - this.view[1] / 2, this.view[0], this.view[1]);
+		for (let i = 0; i < this.clients.length; i++) {
+			this.clients[i].sprite.update(offset);
 		}
 	}
 }
