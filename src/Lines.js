dbad41572b0963a52323c5a9814a88885cefb29c
Owen Roberts <obroberts@gmail.com>
2025-09-09 22:14:12 -0400

    export LINES_VERSION

--- src/Lines.js
+++ src/Lines.js
@@ -11,6 +11,6 @@ import { Drawing } from './Drawing.js';
 import { Animator } from './Animator.js';
 import { LinesAnimation } from './LinesAnimation.js';
 import { Style } from './Style.js';
-import { Points } from './Consts.js';
+import { Points, LINES_VERSION } from './Consts.js';
 
-export { Renderer, PixelMixin, Loader, Layer, AntiMixin, Drawing, Animator, LinesAnimation, Style, Points };
+export { Renderer, PixelMixin, Loader, Layer, AntiMixin, Drawing, Animator, LinesAnimation, Style, Points, LINES_VERSION };
